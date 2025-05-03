e89e255f453fc14c8e4ff3433de9d31a9fd83836
emSz345 <118739243+emSz345@users.noreply.github.com>
2025-05-03 01:50:29 -0300

    Add files via upload

--- src/Page/Login.tsx
+++ src/Page/Login.tsx
@@ -1,34 +1,35 @@
-
 import React, { useState } from "react";
-import { auth, signInWithEmailAndPassword } from '../firebase'; // Importe a função de login do Firebase
+import { 
+  auth, 
+  signInWithEmailAndPassword, 
+  signInWithGoogle, 
+  signInWithFacebook 
+} from '../firebase';
+
 import Input from "../components/Input/Input";
 import Button from "../components/Button/Button";
 import SocialButton from "../components/SocialButton/SocialButton";
-import Header from "../components/Header/Header";
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+
 import logo from "../assets/img-logo.png";
 import googleIcon from "../assets/logo-google.png";
-import appleIcon from "../assets/logo-apple.png";
 import facebookIcon from "../assets/logo-facebook.png";
-import { signInWithGoogle } from '../firebase';
-import { signInWithFacebook, } from "../firebase";
-import { getIdToken } from "firebase/auth";
-import axios from "axios";
+
 import "../styles/Login.css";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [senha, setSenha] = useState<string>("");
+  const [emailError, setEmailError] = useState<string>("");
+  const [senhaError, setSenhaError] = useState<string>("");
+
   const navigate = useNavigate();
 
-  // Função de manipulação de alteração dos inputs
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "senha") {
-      setSenha(value);
-    }
+    if (name === "email") setEmail(value);
+    if (name === "senha") setSenha(value);
   };
 
   const handleGoogleSignIn = async () => {
@@ -41,7 +42,7 @@ const Login: React.FC = () => {
       console.error(error);
     }
   };
-  //DADOS NÃO ESTÃO REGISTRANDO
+
   const handleFacebookSignIn = async () => {
     try {
       await signInWithFacebook();
@@ -54,27 +55,71 @@ const Login: React.FC = () => {
   };
 
   const handleSubmit = async () => {
+    setEmailError("");
+    setSenhaError("");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const senhaForteRegex = /^.{6,}$/;;
+    ;
+
+    // Verificação de campos vazios
+    if (!email || !senha) {
+      if (!email) setEmailError("Digite seu e-mail.");
+      if (!senha) setSenhaError("Digite sua senha.");
+      return;
+    }
+
+    // Validação do e-mail
+    if (email.length < 6 || !emailRegex.test(email)) {
+      setEmailError("Digite um email válido com pelo menos 6 caracteres.");
+      return;
+    }
+
+    if (!senhaForteRegex.test(senha)) {
+      setSenhaError("A senha deve conter pelo menos 6 caractere.");
+      return;
+    }
+    //Token temporarario
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, senha);
-      console.log("Login bem-sucedido!");
+      
+      const user = userCredential.user;
+      const token = await user.getIdToken();
+
+      localStorage.setItem("firebaseToken", token);
+      
+      const response = await axios.get(`http://localhost:5000/api/users/me?email=${user.email}`);
+      localStorage.setItem("userName", response.data.nome || user.email);
+
       alert("Login bem-sucedido!");
       navigate("/Home");
     } catch (error: any) {
-      alert(`Erro ao realizar login. Código: ${error.code}, Mensagem: ${error.message}`);
+      if (error.code === "auth/wrong-password") {
+        setSenhaError("Senha incorreta.");
+      } else if (error.code === "auth/user-not-found") {
+        setEmailError("Usuário não encontrado.");
+      } else {
+        setSenhaError("Erro ao realizar login. Tente novamente.");
+        console.error(error);
+      }
     }
   };
 
   return (
     <div className="login-container">
-      <Header />
+      <header className="header">
+        <Link to="/Home">
+          <img src={logo} alt="Logo" className="header-logo" />
+        </Link>
+      </header>
       <div className="login-content">
         <div className="logo-section">
           <img src={logo} alt="Logo" className="logo-image" />
         </div>
         <div className="form-section">
-          <h2 className="title">Bem-vindo de volta</h2>
+          <h2 className="title">Bem-vindo</h2>
 
-          {/* Inputs para email e senha */}
+          <h3 className="login-title">Email</h3>
           <Input
             type="email"
             name="email"
@@ -82,23 +127,28 @@ const Login: React.FC = () => {
             value={email}
             onChange={handleChange}
           />
+          <div className="login-container-error">
+          {emailError && <p className="error">{emailError}</p>}
+          </div>
+          
+
+          <h3 className="login-title">Senha</h3>
           <Input
             type="password"
             name="senha"
-            placeholder="Password"
+            placeholder="Digite sua senha"
             value={senha}
             onChange={handleChange}
           />
+           <div className="login-container-error">
+           {senhaError && <p className="error">{senhaError}</p>}
+           </div>
+          
 
-          {/* Botão de login */}
-          <Button
-            text="Entrar"
-            onClick={handleSubmit}
-          />
+          <Button text="Entrar" onClick={handleSubmit} />
 
           <p className="ou">ou</p>
 
-          {/* Login social */}
           <div className="social-login">
             <SocialButton icon={googleIcon} alt="Google" onClick={handleGoogleSignIn} />
             <SocialButton icon={facebookIcon} alt="Facebook" onClick={handleFacebookSignIn} />
