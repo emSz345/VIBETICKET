153aff0fdbd241463b41592706e165bce53a2571
felipegabrielsgobicuriel171 <felipegabriel.sgobicuriel@gmail.com>
2025-06-12 17:32:58 -0300

    atualização dos termos

--- src/Page/Public/TermosContent.tsx
+++ src/Page/Public/TermosContent.tsx
@@ -25,7 +25,7 @@ interface TermosProps {
   
           <div className="termos-texto">
             <p>
-              - Nossa plataforma permite que usuários criem eventos, comprem ingressos e realizem
+              Nossa plataforma permite que usuários criem eventos, comprem ingressos e realizem
               doações para instituições. O usuário é responsável por fornecer informações corretas ao
               se cadastrar e deve utilizar o serviço de forma legal e ética. O uso indevido pode levar à
               suspensão ou remoção da conta.
@@ -34,7 +34,7 @@ interface TermosProps {
           <br /><br />
           <div className="termos-texto">
             <p>
-              - Os organizadores são totalmente responsáveis pelos eventos criados, incluindo sua reali-
+              Os organizadores são totalmente responsáveis pelos eventos criados, incluindo sua reali-
               zação, possíveis cancelamentos e reembolsos. A plataforma atua apenas como intermediária
               e não se responsabiliza por problemas nos eventos. Empresas que fizerem grandes doações
               poderão ser destacadas como patrocinadores no site.
@@ -43,7 +43,7 @@ interface TermosProps {
           <br /><br />
           <div className="termos-texto">
             <p>
-              - Para garantir a melhor experiência, armazenamos informações dos usuários em nosso banco
+              Para garantir a melhor experiência, armazenamos informações dos usuários em nosso banco
               de dados de forma segura e protegida. Adotamos medidas para garantir a confidencialidade
               e integridade dos dados, seguindo boas práticas de segurança. Não compartilhamos inform-
               ações pessoais sem consentimento, salvo quando exigido por lei.
@@ -52,7 +52,7 @@ interface TermosProps {
           <br /><br />
           <div className="termos-texto">
             <p>
-              - Nos reservamos o direito de modificar estes termos a qualquer momento, e o uso contínuo
+              Nos reservamos o direito de modificar estes termos a qualquer momento, e o uso contínuo
               da plataforma após atualizações implica na aceitação das novas condições. Para dúvidas
               ou suporte, entre em contato pelo e-mail [e-mail de contato].
             </p>
